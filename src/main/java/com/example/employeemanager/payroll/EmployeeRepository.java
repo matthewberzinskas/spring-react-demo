d7d32d961c86ae5967a30c1a86bdd01892d7abe8
matthewberzinskas <pasomatt@gmail.com>
2022-08-01 14:34:31 -0700

    Setup employee repository to extend Crud repo.

---
+++ src/main/java/com/example/employeemanager/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.employeemanager.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
